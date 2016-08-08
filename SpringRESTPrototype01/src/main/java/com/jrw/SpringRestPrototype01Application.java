906734222246068cc8912af25cf18ce6468340ef
J Woodruff <jamesrwoodruff@gmail.com>
2016-08-08 11:42:38 -0700

    initial commit

---
+++ SpringRESTPrototype01/src/main/java/com/jrw/SpringRestPrototype01Application.java
@@ -0,0 +1,12 @@
+package com.jrw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestPrototype01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestPrototype01Application.class, args);
+	}
+}
